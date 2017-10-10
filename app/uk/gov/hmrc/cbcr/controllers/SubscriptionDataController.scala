704f9e5c01b5d7059198d05c3dab453b9bb30802
Max Worgan <max.worgan@digital.hmrc.gov.uk>
2017-10-10 16:50:42 +0100

    Moved routes to test-only routes

--- app/uk/gov/hmrc/cbcr/controllers/SubscriptionDataController.scala
+++ app/uk/gov/hmrc/cbcr/controllers/SubscriptionDataController.scala
@@ -90,7 +90,7 @@ class SubscriptionDataController @Inject() (repo:SubscriptionDataRepository,des:
     )
   }, parse.json)
 
-  def clearSubscriptionData(cbcId: CBCId): Action[AnyContent] = auth.authCBCR { implicit request =>
+  def clearSubscriptionData(cbcId: CBCId): Action[AnyContent] = Action.async{ implicit request =>
     repo.clear(cbcId).cata[Result](
       NotFound,
       result => if (!result.ok) InternalServerError(result.writeErrors.mkString) else Ok("ok")
