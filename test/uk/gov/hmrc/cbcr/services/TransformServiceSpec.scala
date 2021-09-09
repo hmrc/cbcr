e370068d15e514b40a2eae9ba722b1c4084cdda2
soljeggers <35451762+soljeggers@users.noreply.github.com>
2021-09-09 16:51:28 +0100

    DAC6-1021 - added namespace definitions to transformation service

--- test/uk/gov/hmrc/cbcr/services/TransformServiceSpec.scala
+++ test/uk/gov/hmrc/cbcr/services/TransformServiceSpec.scala
@@ -31,29 +31,92 @@ class TransformServiceSpec extends SpecBase with StreamlinedXmlEquality {
       val service = app.injector.instanceOf[TransformService]
 
       val file =
-        <CBCRSubmissionInboundRequest xmlns:dac6="urn:eu:taxud:cbcr:v1"
-                                        xmlns:eis="http://www.hmrc.gov.uk/cbcr/eis"
+        <CBCSubmissionInboundRequest xmlns:cbc="urn:eu:taxud:cbc:v1"
+                                        xmlns:eis="http://www.hmrc.gov.uk/cbc/eis"
                                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                                        xsi:schemaLocation="http://www.hmrc.gov.uk/cbcr/eis/CBCR_EIS_UK_schema.xsd">
+                                        xsi:schemaLocation="http://www.hmrc.gov.uk/cbc/eis/CBC_EIS_UK_schema.xsd">
           <file>
             <result>true</result>
           </file>
-        </CBCRSubmissionInboundRequest>
+        </CBCSubmissionInboundRequest>
 
       val expected =
-        <eis:CBCRSubmissionInboundRequest xmlns:dac6="urn:eu:taxud:cbcr:v1"
-                                            xmlns:eis="http://www.hmrc.gov.uk/cbcr/eis"
+        <eis:CBCSubmissionInboundRequest xmlns:cbc="urn:eu:taxud:cbc:v1"
+                                            xmlns:eis="http://www.hmrc.gov.uk/cbc/eis"
                                             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                                            xsi:schemaLocation="http://www.hmrc.gov.uk/cbcr/eis/CBCR_EIS_UK_schema.xsd">
+                                            xsi:schemaLocation="http://www.hmrc.gov.uk/cbc/eis/CBC_EIS_UK_schema.xsd">
           <eis:file>
             <eis:result>true</eis:result>
           </eis:file>
-        </eis:CBCRSubmissionInboundRequest>
+        </eis:CBCSubmissionInboundRequest>
 
-      val result: NodeSeq = service.addNameSpaces(file, Seq(NamespaceForNode("CBCRSubmissionInboundRequest", "eis")))
+      val result: NodeSeq = service.addNameSpaces(file, Seq(NamespaceForNode("CBCSubmissionInboundRequest", "eis")))
 
       result shouldBe expected
 
+    }
+
+    "handle two Namespaces in a file" in {
+      val service = app.injector.instanceOf[TransformService]
+      val file =
+        <CBCSubmissionInboundRequest xmlns:cbc="urn:eu:taxud:cbc:v1"
+                                        xmlns:eis="http://www.hmrc.gov.uk/cbc/eis"
+                                        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                                        xsi:schemaLocation="http://www.hmrc.gov.uk/cbc/eis/CBC_EIS_UK_schema.xsd">
+          <file>
+            <result>true</result>
+            <requestDetail>
+              <CBC_Report xmlns:cbc="urn:cbc:v0.1"
+                                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                                version="2020-04-16T09:30:47Z">
+                <submission>Submitted Data</submission>
+              </CBC_Report>
+            </requestDetail>
+          </file>
+        </CBCSubmissionInboundRequest>
+
+      val expected =
+        <eis:CBCSubmissionInboundRequest xmlns:cbc="urn:eu:taxud:cbc:v1"
+                                            xmlns:eis="http://www.hmrc.gov.uk/cbc/eis"
+                                            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                                            xsi:schemaLocation="http://www.hmrc.gov.uk/cbc/eis/CBC_EIS_UK_schema.xsd">
+          <eis:file>
+            <eis:result>true</eis:result>
+            <eis:requestDetail>
+              <cbc:CBC_Report xmlns:cbc="urn:cbc:v0.1"
+                                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                                     version="2020-04-16T09:30:47Z">
+                <cbc:submission>Submitted Data</cbc:submission>
+              </cbc:CBC_Report>
+            </eis:requestDetail>
+          </eis:file>
+        </eis:CBCSubmissionInboundRequest>
+
+      val result: NodeSeq = service.addNameSpaces(
+        file,
+        Seq(
+          NamespaceForNode("CBCSubmissionInboundRequest", "eis"),
+          NamespaceForNode("CBC_Report", "cbc")
+        ))
+
+      result shouldBe expected
+    }
+
+    "add namespace definitions for a CBCR Submission" in {
+      val service = app.injector.instanceOf[TransformService]
+      val file = <CBC_Report version="2020-04-16T09:30:47Z">
+        <submission>Submitted Data</submission>
+      </CBC_Report>
+
+      val expected = <CBC_Report version="2020-04-16T09:30:47Z"
+                                       xmlns:cbc="urn:cbc:v0.1"
+                                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <submission>Submitted Data</submission>
+      </CBC_Report>
+
+      val result = service.addNameSpaceDefinitions(file)
+
+      result.toString shouldBe expected.toString
     }
   }
 }
