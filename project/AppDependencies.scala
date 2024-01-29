b923ecc7e56518f127dc11a2c4cddd94481ec111
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-01-29 15:26:17 +0000

    DLS-9710: Tidied up test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,6 @@ import sbt.*
 
 object AppDependencies {
   val bootstrapVersion = "7.23.0"
-  val hmrcMongoVersion = "0.74.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -11,21 +10,15 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "domain"                    % "8.1.0-play-28",
     "org.typelevel"     %% "cats-core"                 % "2.0.0",
     "uk.gov.hmrc"       %% "emailaddress"              % "3.7.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.74.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion % scope,
-    "com.typesafe.akka"      %% "akka-testkit"             % "2.6.21"         % scope,
-    "org.scalatest"          %% "scalatest"                % "3.0.9"          % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"              % "0.35.10"        % scope,
-    "org.pegdown"            % "pegdown"                   % "1.6.0"          % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.0.0"          % scope,
-    "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"    % scope,
-    "org.mockito"            % "mockito-core"              % "3.11.2"         % scope,
-    "org.scalacheck"         %% "scalacheck"               % "1.15.0"         % scope,
-    "com.github.tomakehurst" % "wiremock-standalone"       % "2.25.0"         % scope,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion % scope,
-    "org.scalatestplus"      %% "scalatestplus-mockito"    % "1.0.0-M2"       % scope,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % bootstrapVersion % scope,
+    "com.typesafe.akka" %% "akka-testkit"             % "2.6.21"         % scope,
+    "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"    % scope,
+    "org.mockito"       % "mockito-core"              % "3.11.2"         % scope,
+    "org.scalacheck"    %% "scalacheck"               % "1.15.0"         % scope,
+    "org.scalatestplus" %% "scalatestplus-mockito"    % "1.0.0-M2"       % scope
   )
 }
