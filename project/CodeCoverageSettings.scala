b32cdd74ec28c7d8302c018e9212682c64ce5795
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2024-03-20 16:55:49 +0000

    cause pr builder to break

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -33,7 +33,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 80,
+    ScoverageKeys.coverageMinimumStmtTotal := 100,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
