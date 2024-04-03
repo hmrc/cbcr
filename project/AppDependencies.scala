bfa1c62b690edd271eaa47de2b1c31c3e4a829c5
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-04-03 17:08:50 +0100

    DLS-10111 Remove unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,13 +9,8 @@ object AppDependencies {
     "uk.gov.hmrc"              %% "bootstrap-backend-play-28"  % bootstrapVersion,
     "uk.gov.hmrc"              %% "domain"                     % "8.1.0-play-28",
     "org.typelevel"            %% "cats-core"                  % "2.0.0" exclude("org.scalacheck","scalacheck_2.12"),
-    "com.github.kxbmap"        %% "configs"                    % "0.6.0",
     "uk.gov.hmrc"              %% "emailaddress"               % "3.7.0",
-    "uk.gov.hmrc.mongo"        %% "hmrc-mongo-play-28"         % "0.74.0",
-    "org.codehaus.woodstox"    %  "woodstox-core-asl"          % "4.4.1",
-    "msv"                      %  "msv"                        % "20050913",
-    "com.sun.xml"              %  "relaxngDatatype"            % "1.0",
-    "com.sun.msv.datatype.xsd" %  "xsdlib"                     % "2013.2"
+    "uk.gov.hmrc.mongo"        %% "hmrc-mongo-play-28"         % "0.74.0"
   )
 
   def test = Seq(
@@ -23,12 +18,10 @@ object AppDependencies {
     "com.typesafe.akka"       %% "akka-testkit"             % "2.6.21"         % Test,
     "org.scalatest"           %% "scalatest"                % "3.0.9"          % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"        % Test,
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"          % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.0.0"          % Test,
     "org.scalatestplus"       %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"    % Test,
     "org.mockito"             %  "mockito-core"             % "3.11.2"         % Test,
     "org.scalacheck"          %% "scalacheck"               % "1.15.0"         % Test,
-    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.25.0"         % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.74.0"         % Test,
     "org.scalatestplus"       %% "scalatestplus-mockito"    % "1.0.0-M2"       % Test,
   )
