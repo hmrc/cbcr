dae7cd62632061f4d4d3efd1a2031b26eca18609
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-04-04 15:04:32 +0100

    DLS-10111 Remove dependency on AkkaTestKit

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,6 @@ object AppDependencies {
 
   def test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion % Test,
-    "com.typesafe.akka"       %% "akka-testkit"             % "2.6.21"         % Test,
     "org.scalatest"           %% "scalatest"                % "3.0.9"          % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"        % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.0.0"          % Test,
